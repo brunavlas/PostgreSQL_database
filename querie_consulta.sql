05d79771f11c2894c44f8cd9713b6a5d1d992a48
Bruna Salvan <bruna.salvan@hotmail.com>
2022-08-12 17:22:30 -0300

    Update querie_consulta.sql

--- querie_consulta.sql
+++ querie_consulta.sql
@@ -1,6 +1,6 @@
 select l.user_id, l.usuario, u.nome, u.sobrenome, u.login, max(l.created_date)
-from gobuzz."TB_LOGIN_IPS" l
-inner join gobuzz."TB_USERS" u
+from teste1."TB_LOGIN_IPS" l
+inner join teste1."TB_USERS" u
 on u.id = l.user_id
 where origem_ip = 'G'
 group by l.user_id, l.usuario, u.nome, u.sobrenome, u.login
