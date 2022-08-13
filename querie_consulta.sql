8843096e3af302a8c8cd8932eb5f9e4c365383d5
Bruna Salvan <bruna.salvan@hotmail.com>
2022-08-13 12:29:27 -0300

    Update querie_consulta.sql

--- querie_consulta.sql
+++ querie_consulta.sql
@@ -1,3 +1,9 @@
+/*  Na query foi utilizada a função 'max' para trazer o valor máximo da coluna 'created_time' visando recuperar o registro mais recente para cada usuário;
+'Join' para unir as duas tabelas;
+'Where' para aplicar um filtro, que nesse caso seria recuperar apenas registros em que o IP fosse 'G';
+'Group by' para agrupar os registros, já que foi utilizada uma função de agregação;
+'Order by' para ordenar por 'created_date' para recuperar os últimos acessos com aquele determinado IP. */
+
 select l.user_id, l.usuario, u.nome, u.sobrenome, u.login, max(l.created_date)
 from teste1."TB_LOGIN_IPS" l
 inner join teste1."TB_USERS" u
